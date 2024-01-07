4c12244c4fd4c0deab6497dd459795ec854297e1
quockhanhnguyentruong <nhokbjn1997@gmail.com>
2024-01-07 21:07:39 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [authorization, setAuthorization] = useState('');
+  const [chatID, setChatID] = useState('');
+  const [message, setMessage] = useState('');
+
+  const makeAPICall = async () => {
+    try {
+      const response = await axios.post(
+        `https://discord.com/api/v9/channels/${chatID}/messages`,
+        {
+          content: message,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: authorization,
+          },
+        }
+      );
+      console.log('API Response send message successful: ', response.data.id);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  const handleSendMessage = () => {
+    makeAPICall();
+    // You can add additional logic or reset the message input after sending.
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>
+        Enter Authorization:
+        <input
+          type="text"
+          value={authorization}
+          onChange={(e) => setAuthorization(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Enter chatID:
+        <input
+          type="text"
+          value={chatID}
+          onChange={(e) => setChatID(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Enter Message:
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </label>
+      <br />
+      <button onClick={handleSendMessage}>Send Message</button>
     </div>
   );
 }
